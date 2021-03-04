023d283c26f4fbe9edd12bcd42319ee49108c1a9
OmarAliSoliman <omarasmar9955@gmail.com>
2021-03-04 13:17:45 +0200

    first

---
+++ assets/js/main.js
@@ -0,0 +1,95 @@
+$(document).ready(function () {
+  var currentDir = $("a").css("direction");
+
+  if ($(".news-slider").length) {
+    $(".news-slider").slick({
+      dots: true,
+      infinite: false,
+      speed: 400,
+      autoplaySpeed: 1000,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      rtl: currentDir == "rtl" ? true : false,
+      loop: true,
+      arrows: false,
+      infinite: true,
+      cssEase: 'linear',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+          },
+        },
+      ],
+    });
+  }
+
+  if ($(".companies-slider").length) {
+    $(".companies-slider").slick({
+      dots: true,
+      infinite: false,
+      speed: 400,
+      autoplaySpeed: 1000,
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      rtl: currentDir == "rtl" ? true : false,
+      loop: true,
+      arrows: false,
+      infinite: true,
+      cssEase: 'linear',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+          },
+        },
+      ],
+    });
+  }
+
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+});
